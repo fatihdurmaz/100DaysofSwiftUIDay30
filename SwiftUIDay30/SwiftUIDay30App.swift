aae40e455f6d12befa0ca4134b864e844fc6aac8
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-01-23 23:16:59 +0300

    Initial Commit

---
+++ SwiftUIDay30/SwiftUIDay30App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDay30App.swift
+//  SwiftUIDay30
+//
+//  Created by Fatih Durmaz on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDay30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
